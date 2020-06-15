456d0e95070967e2d0dbd95a0ba4e842ec8c5f2b
Andrey Faychak <faychak333@gmail.com>
2020-06-15 12:30:26 +0300

    Updates

---
+++ precache-manifest.f8ac78659af994863454043033202cf4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae500da8a9cc4208bf4861b0287ddcae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "040388f6ef4d5b46a3f9",
+    "url": "./static/css/main.5734fa6e.chunk.css"
+  },
+  {
+    "revision": "8c3a953cf7cbeaefd5d7",
+    "url": "./static/js/2.3763b5c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3763b5c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040388f6ef4d5b46a3f9",
+    "url": "./static/js/main.2389f4cd.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "0dfb20cd3cd1a674fec3bfc0e95e2651",
+    "url": "./static/media/favorite-hover.0dfb20cd.svg"
+  },
+  {
+    "revision": "45102a14944f1b164a553e0cc3650796",
+    "url": "./static/media/favorite.45102a14.svg"
+  }
+]);
